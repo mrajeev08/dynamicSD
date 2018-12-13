ac5cc94c2bbb3ff20b0c85a4f597353ce92c86d0
Malavika Rajeev <mrajeev08@gmail.com>
2018-12-13 02:10:06 -0500

    older vers...

--- profiling_IBM.R
+++ profiling_IBM.R
@@ -54,7 +54,7 @@ R_0 = 1.2; k = 0.2;
 iota = 1; scale_iota = 1;
 # weekly number of incursions and scaling factor 
 sigma = get.prob(rate = 7/22.3, step = 1); # weekly rate to prob exp -> inf
-births = get.prob(rate = grid_data$births, step = 52); # annual birth rate to prob
+data$births = get.prob(rate = grid_data$births, step = 52); # annual birth rate to prob (need to rework this!)
 mu = get.prob(rate = 0.44, step = 52); # annual death rate to prob 
 ntimes = tmax; # maximum time step
 nu = get.prob(rate = 0.33, step = 52); 
@@ -66,131 +66,158 @@ dispersalShape = 0.3484; dispersalScale = 41.28/100;
 # vacc_mat[,2:ncol(vacc_mat)] <- ifelse(vacc_mat[,2:ncol(vacc_mat)] > 0, 0, 0)
 # start_vacc = 0
 
-# 1. Set-up and tstep 1 -----------------------------------------------------------------------
+# PRE SET-UP, should be done outside of function! --------------------------------------------------
+grid_ID <- grid ## set up ID grid
+values(grid_ID) <- data$cell_id ## cell IDs as values
+
+## create coordinates of raster
+coords <- coordinates(grid)
+data$x_coord <- coords[, 1]
+data$y_coord <- coords[, 2]
+
+## Filter to ones inside SD villages 
+data <- filter(data, !is.na(village_ID) & !is.na(start_pop)) # subset out ones not in SD vills
+## also subsetting out empty cells...need to think if I NEED to populate these somehow...
 nlocs <- nrow(data)
+row_id <- 1:nlocs
+cell_id <- data$cell_id
+births <- data$births
 
 ## rep incursions (so as to include scaling factor)
 inc_rate <- ifelse(length(scale_iota) > 1, iota*scale, rep(iota, length(tmax)))
 
+# 1. Set-up and tstep 1 -----------------------------------------------------------------------
 # state matrices
-S <- V <- N <- matrix(NA, nrow = nlocs, ncol = ntimes)
+S <- V <- N <- matrix(0, nrow = nlocs, ncol = ntimes)
 I_dist <- I_all <- E <- matrix(0, nrow = nlocs, ncol = ntimes)
-
-## create coordinates of raster and get min + max values
-coords <- coordinates(grid)
-x_min <- min(coords[, 1])
-x_max <- max(coords[, 1])
-y_max <- max(coords[, 2])
-y_min <- min(coords[, 2])
 
 ## Starting pop + sus 
 N[, 1] <- data$start_pop
 V[, 1] <- rbinom(n = nlocs, size = data$start_pop, prob = start_vacc)
 S[, 1] <- N[, 1] - V[, 1]
-sus_rast <- grid
-
 
 # 2. Simulating infection in first timestep ------------------------------------------------------
 # Seeding cases (as if from outside!)
 # pick random location to start I seeds
 I_dist[ ,1] <- 0
-I_locs <- sample((1:nlocs)[which(!is.na(data$start_pop))], I_seed)
+I_locs <- sample((row_id)[which(!is.na(data$start_pop))], I_seed)
 I_dist[I_locs, 1] <- 1
 
 ## Create infected coordinate data frame to grow
-I_coords <- data.frame(ID = 1:I_seed, tstep = rep(1, I_seed), x_coord = coords[I_locs, 1], 
-                       y_coord = coords[I_locs, 2], progen_ID = rep(0, I_seed),
-                       path_ID = rep(NA, I_seed), sus = NA, trans = NA,
+I_coords <- data.frame(ID = 1:I_seed, tstep = rep(1, I_seed), x_coord = data$x_coord[I_locs], 
+                       y_coord = data$y_coord[I_locs], progen_ID = rep(0, I_seed),
+                       path_ID = rep(NA, I_seed), cell_id = cell_id[I_locs], 
+                       sus = NA, trans = NA,
                        infectious = rep(1, I_seed), secondaries = NA)
 
 ## Create exposed coordinate data frame to grow
 E_coords <- data.frame(ID = NA, tstep = NA, x_coord = NA, y_coord = NA, 
-                       progen_ID = NA, path_ID = NA, sus = NA, trans = NA, 
+                       progen_ID = NA, path_ID = NA, cell_id = NA, sus = NA, trans = NA, 
                        infectious = NA, secondaries = NA)
-out <- E_coords ## for keeping track of ones that go outside and tstep E_coords
 
-I_coords$secondaries <- rnbinom(nrow(I_coords), mu = R_0, size = k)
 counter <- length(I_seed) ## for keeping track of ids
 
-for (i in 1:nrow(I_coords)){
-  if (I_coords$secondaries[i] > 0) {
-    for (j in 1:I_coords$secondaries[i]){ 
-      counter <- counter + 1
-      if (j == 1) { # need progenitor coords for 1st movement
-        origin_x <- I_coords$x_coord[i]
-        origin_y <- I_coords$y_coord[i]
-      } else {
-        origin_x <- E_coords$x[nrow(E_coords)]
-        origin_y <- E_coords$y[nrow(E_coords)]
+if(nrow(I_coords) > 0){
+  I_coords$secondaries <- rnbinom(nrow(I_coords), mu = R_0, size = k)
+  for (i in 1:nrow(I_coords)){
+    if (I_coords$secondaries[i] > 0) {
+      for (j in 1:I_coords$secondaries[i]){ 
+        counter <- counter + 1
+        if (j == 1) { # need progenitor coords for 1st movement
+          origin_x <- I_coords$x_coord[i]
+          origin_y <- I_coords$y_coord[i]
+        } else {
+          origin_x <- last_coords[1]
+          origin_y <- last_coords[2]
+        }
+        within <- 0
+        while (within == 0) {
+          distance <- rweibull(1, shape = dispersalShape, 
+                               scale = dispersalScale) # Distance to move in km
+          angle <- runif(n = 1, min = 0, max = 2*pi)  # Angle to move at
+          
+          # Convert to a vector and move
+          x_new <- (sin(angle) * distance * 1000) + origin_x # convert to m
+          y_new <- (cos(angle) * distance * 1000) + origin_y
+          
+          ## This means will only go somewhere within a vill
+          check <- extract(grid_ID, cbind(x_new, y_new))
+          within <- ifelse(check %in% cell_id, 1, 0)
+          ## This code means only in populated places and doggies don't leave the district
+        }
+        
+        E_coords_new <- data.frame(ID = counter, tstep = 1, 
+                                   x_coord = x_new, y_coord = y_new,  
+                                   progen_ID = I_coords$ID[i], path_ID = j, 
+                                   cell_id = check, sus = NA, trans = 0, 
+                                   infectious = NA, secondaries = NA)
+        last_coords <- c(x_new, y_new)
+        E_coords <- rbind(E_coords, E_coords_new)
       }
-      distance <- rweibull(1, shape = dispersalShape, 
-                           scale = dispersalScale) # Distance to move in km
-      angle <- runif(n = 1, min = 0, max = 2*pi)  # Angle to move at
-      
-      # Convert to a vector and move
-      x_new <- (sin(angle) * distance * 1000) + origin_x # convert to m
-      y_new <- (cos(angle) * distance * 1000) + origin_y
-      
-      E_coords_new <- data.frame(ID = counter, tstep = 1, 
-                                 x_coord = x_new, y_coord = y_new,  
-                                 progen_ID = I_coords$ID[i], path_ID = j,
-                                 sus = NA, trans = NA, infectious = NA, secondaries = NA)
-      E_coords <- rbind(E_coords, E_coords_new)
-    } 
-      
-    ## Potentially don't need this as we wont have sus for the points outside the district
-    out <- rbind(out, filter(E_coords, x_coord <= x_min | x_coord >= x_max |
-                               y_coord <= y_min | y_coord >= y_max))
-    E_coords_now <- filter(E_coords, tstep == 1, x_coord >= x_min, x_coord <= x_max,
-                           y_coord >= y_min, y_coord <= y_max) ## filter out ones outside
-    values(sus_rast) <- S[, 1]/N[, 1] ## probability that contact will be with a susceptible
-    
-    E_coords_now$sus <- extract(sus_rast, cbind(E_coords_now$x_coord, E_coords_now$y_coord))
-    E_coords_now$trans <- rbinom(nrow(E_coords_now), size = 1, prob = E_coords_now$sus)
-    E_coords_now %>%
-      drop_na(trans) %>%
-      filter(trans == 1) -> E_coords
-  } else
-    E_coords_now <- filter(E_coords, tstep == -1)
+    }
+  }
 }
 
-if(nrow(E_coords) > 1) {
-  E[, 1] <- values(rasterize(cbind(E_coords$x_coord, E_coords$y_coord), grid, 
-                             fun= function(x,...)length(x)))
-  E[is.na(E[,1]), 1] <- 0
+E_coords <- filter(E_coords, !is.na(ID))
+
+if(nrow(E_coords) > 0) {
+  ## probability that contact will be with a susceptible = St/Nt-1
+  E_coords$sus <- S[row_id[match(E_coords$cell_id, cell_id)], 1]/
+    N[row_id[match(E_coords$cell_id, cell_id)], 1]
+  E_coords$trans <- rbinom(nrow(E_coords), size = 1, prob = E_coords$sus)
+  E_coords %>%
+    drop_na(trans) %>%
+    filter(trans == 1) -> E_coords
 } else {
-  E[, 1] <- 0
+  E_coords <- E_coords # to get empty df to bind!
+}
+
+if (nrow(E_coords) > 0) {
+  E_coords %>%
+    group_by(cell_id) %>%
+    summarize(count = n()) -> E_count
+  E[row_id[match(E_count$cell_id, cell_id)], 1] <- E_count$count ## add in newly exposed
 }
 
 ## Finish subtracting everyone out
 S[, 1] <- S[, 1] - E[ ,1] ## subtract out newly exposed guys from S
+unaccounted <- rep(0, ntimes)
 
 # 3. Simulate for rest of time steps -------------------------------------------------------------
 for (t in 2:ntimes) {
-  ## t = 2
   print(paste(t, "/", tmax, "weeks"))
+  # t = 81
   # 3a. Simulate vaccination ------------------------------------------------------------------------
   ## TO DO: ## speed up not using filter
   ## TO DO: ## make sure leftover vaccinated goes to adjacent vills
   data %>%
-    mutate(S = S[, t-1], V = V[, t-1]) %>%
-    dplyr::select(villcode, S, V) %>%
+    mutate(S_vill = S[, t-1], V_vill = V[, t-1]) %>%
+    dplyr::select(villcode, S_vill, V_vill) %>%
     group_by(villcode) %>%
     summarize_all(sum, na.rm = TRUE) -> now
   vacc %>%
     dplyr::select(villcode, vacc = t + 1) %>%
     left_join(now) %>%
-    mutate(p_vacc = ifelse(vacc - p_revacc*V <= 0, vacc/S,
-                           (vacc - p_revacc*V)/S)) %>%
-    mutate(p_vacc = ifelse(p_vacc > 1, 1, p_vacc)) %>% 
+    mutate(revacc = rbinom(nrow(.), size = V_vill, prob = p_revacc),
+           vacc_new = ifelse(vacc - revacc <= 0, vacc, vacc - revacc), 
+           ## if revacc is really big then probably an additive campaign!
+           vacc_new = ifelse(vacc_new > S_vill, S_vill, vacc_new)) %>%
     right_join(data) %>%
-    mutate(n_vacc = rbinom(nlocs, size = S, prob = p_vacc*prop_pop)) %>%
-    replace_na(list(n_vacc = 0)) -> vacc_now
+    mutate(S_loc = S[, t-1], V_loc = V[, t-1]) %>%
+    group_by(villcode) %>%
+    mutate(vacc_prob = ifelse(vacc_new == 0, 0, S_loc/vacc_new), 
+             n_vacc = ifelse(vacc_new == 0, 0, 
+                             rmultinom(1, size = first(vacc_new), prob = vacc_prob)),
+             n_vacc = ifelse(n_vacc - S_loc > 0, S_loc, n_vacc),
+             leftovers = ifelse(n_vacc - S_loc > 0, n_vacc - S_loc, 0)) -> vacc_now
+    unaccounted[t] <- sum(vacc_now$leftovers, na.rm = TRUE)
+    
+    ## ends up in same order (try length(vacc_now$cell_ID == data$cell_ID)[FALSE], should = 0)
   
-  # 3b. Sequential competing transitions for S + V -----------------------------------------------
+  # 3b. Sequential transitions for S + V -----------------------------------------------
   ## S transitions, sequenctially for competing probs (-vacc, - deaths)
-  S[, t] <- S[, t-1] - vacc_now$n_vacc ## - vaccinated
-  S[, t] <- S[, t] - rbinom(nlocs, size = S[, t], prob = mu)
+  S[, t] <- S[, t-1] - vacc_now$n_vacc ## - vaccinated first
+  S[, t] <- S[, t] - rbinom(nlocs, size = S[, t], prob = mu) ## then die
   
   ## V transitions, sequetially for competing probs (-waning, - deaths)
   waning <- rbinom(nlocs, size = V[, t-1], prob = nu)
@@ -211,12 +238,11 @@ for (t in 2:ntimes) {
   
   ## Only count exposed -> infectious from within to subtract out
   if(nrow(I_coords_in) > 0) {
-    I_dist[, t] <- values(rasterize(cbind(I_coords_in$x_coord, I_coords_in$y_coord), grid, 
-                             fun= function(x,...)length(x)))
-    I_dist[is.na(I_dist[, t]), t] <- 0 
-  } else {
-    I_dist[, t] <- 0
-  }
+    I_coords_in %>%
+      group_by(cell_id) %>%
+      summarize(count = n()) -> I_count
+    I_dist[row_id[match(I_count$cell_id, cell_id)], t] <- I_count$count
+  } 
   
   E_coords %>% 
     drop_na(infectious) %>%
@@ -231,14 +257,17 @@ for (t in 2:ntimes) {
     I_all[, t] <- 0
     
     ## only happen in populated places
-    I_locs <- sample((1:nlocs)[which(!is.na(data$start_pop))], incursions) 
+    I_locs <- sample((row_id)[which(!is.na(data$start_pop))], incursions) 
     I_all[I_locs, t] <- 1 
     
-    I_coords_out <- data.frame(ID = start_counter:counter, tstep = t, x_coord = coords[I_locs, 1],
-                                y_coord = coords[I_locs, 2], progen_ID = 0, path_ID = NA, sus = NA, 
+    I_coords_out <- data.frame(ID = start_counter:counter, tstep = t, x_coord = data$x_coord[I_locs],
+                                y_coord = data$y_coord[I_locs], progen_ID = 0, path_ID = NA, 
+                                cell_id = data$cell_id[I_locs], sus = NA,
                                 trans = NA, infectious = 1, 
                                 secondaries = NA)
     I_coords_now <- rbind(I_coords_in, I_coords_out)
+  } else{
+    I_coords_now <- I_coords_in
   }
   
   I_all[, t] <-I_all[, t] + I_dist[, t]
@@ -261,8 +290,8 @@ for (t in 2:ntimes) {
             origin_x <- last_coords[1]
             origin_y <- last_coords[2]
           }
-          check <- NA
-          while (is.na(check)) {
+          within <- 0
+          while (within == 0) {
             distance <- rweibull(1, shape = dispersalShape, 
                                scale = dispersalScale) # Distance to move in km
             angle <- runif(n = 1, min = 0, max = 2*pi)  # Angle to move at
@@ -272,14 +301,15 @@ for (t in 2:ntimes) {
             y_new <- (cos(angle) * distance * 1000) + origin_y
             
             ## This means will only go somewhere within a vill
-            check <- extract(grid, cbind(x_new, y_new))
-            ## TO DO ##: Need some code in here so that it goes to populated place!
+            check <- extract(grid_ID, cbind(x_new, y_new))
+            within <- ifelse(check %in% data$cell_id == TRUE, 1, 0)
+            ## This code means only in populated places and doggies don't leave the district
           }
           
           E_coords_new <- data.frame(ID = counter, tstep = t, 
                                      x_coord = x_new, y_coord = y_new,  
                                      progen_ID = I_coords_now$ID[i], path_ID = j, 
-                                     sus = NA, trans = NA, 
+                                     cell_id = check, sus = NA, trans = 0, 
                                      infectious = NA, secondaries = NA)
           last_coords <- c(x_new, y_new)
           E_coords_now <- rbind(E_coords_now, E_coords_new)
@@ -290,37 +320,53 @@ for (t in 2:ntimes) {
     E_coords_now <- I_coords_now ## gets you empty df
   }
   
-  # out <- rbind(out, filter(E_coords_now, x_coord <= x_min | x_coord >= x_max |
-  #                            y_coord <= y_min | y_coord >= y_max)) # store ones that went outside
-  # E_coords_now <- filter(E_coords_now, x_coord >= x_min, x_coord <= x_max,
-  #                   y_coord >= y_min, y_coord <= y_max) ## filter out ones where S[,t] is NA
-  
   # 3f. Aggregate probability that contact is with susceptible -----------------------------------
+  E[, t] <- E[, t-1] - I_dist[, t] ## - infectious from within district + newly exposed
+  N[, t] <- S[, t] + V[, t] + E[, t] + I_dist[, t]
   
   if(nrow(E_coords_now) > 0) {
-    values(sus_rast) <- S[, t]/(S[, t] + E[, t-1] + V[, t])   
-    ## probability that contact will be with a susceptible, E[,t-1] = prob with I_dist[,t] too
-    E_coords_now$sus <- extract(sus_rast, cbind(E_coords_now$x_coord, E_coords_now$y_coord))
-    E_coords_now$trans <- rbinom(nrow(E_coords_now), size = 1, prob = E_coords_now$sus)
+    ## probability that contact will be with a susceptible = St/Nt-1
+    E_coords_now$sus <- S[row_id[match(E_coords_now$cell_id, cell_id)], t]
+    E_coords_now$N <- N[row_id[match(E_coords_now$cell_id, cell_id)], t]
+    ids <-  unique(E_coords_now$cell_id)
+    for (i in 1:length(ids)) {
+      E_trans <- filter(E_coords_now, cell_id == ids[i])
+      done <- 0
+      max <- E_trans$sus[1]
+      for(j in 1:nrow(E_trans)){
+        while(done < max){
+          E_trans$trans[j] <- rbinom(1, size = 1, prob = ifelse(E_trans$N[j] == 0, 0, 
+                                                                E_trans$sus/E_trans$N[j]))
+          E_coords_now$trans[E_coords_now$ID %in% E_trans$ID[j]] <- E_trans$trans[j]
+          done <- done + E_trans$trans[j]
+        }
+      }
+    }
+    
     E_coords_now %>%
+      mutate(sus = sus/N) %>%
+      dplyr::select(-N) %>%
       drop_na(trans) %>%
       filter(trans == 1) -> E_coords_now
   } else {
     E_coords_now <- E_coords_now # to get empty df to bind!
   }
-  
+
   if (nrow(E_coords_now) > 0) {
-    E_new <- values(rasterize(cbind(E_coords_now$x_coord, E_coords_now$y_coord), 
-                              grid, fun= function(x,...)length(x)))
-    E_new[is.na(E_new)] <- 0
+    E_coords_now %>%
+      group_by(cell_id) %>%
+      summarize(count = n()) -> E_count
+    E_new <- rep(0, nlocs)
+    E_new[row_id[match(E_coords_now$cell_id, cell_id)]] <- E_count$count ## add in newly exposed
+    E_new <- ifelse(E_new > S[, t], S[, t], E_new) ## cannot have more exposed than available sus!
+    ## Alternatively add to above where you get sus + n and do group_by cell_id (more realistic + keep track of trans)
   } else {
     E_new <- rep(0, nlocs)
   }
   
   ## Finish subtracting everyone out
-  E[, t] <- E[, t-1] + E_new - I_dist[, t] ## - infectious from within district + newly exposed
+  E[, t] <- E[, t] + E_new ## - infectious from within district + newly exposed
   S[, t] <- S[, t] - E_new ## subtract out newly exposed guys in the week
-  N[, t] <- S[, t] + V[, t] + E[, t] + I_dist[, t]
   
   E_coords <- rbind(E_coords, E_coords_now)
   
@@ -335,6 +381,6 @@ sum_times <- function(vector, steps, na.rm=TRUE) {    # 'matrix'
 }
 
 mIobs <- apply(I_all[,1:(ncol(I_all)-3)], 1 , sum_times, steps = 4)
-plot(rowSums(mIobs[ ,!is.na(grid_data$village_ID)], na.rm = TRUE), type = "l")
-plot(colSums(I_all[!is.na(grid_data$village_ID), ], na.rm = TRUE), type = "l")
+plot(rowSums(mIobs, na.rm = TRUE), type = "l")
+plot(colSums(I_all, na.rm = TRUE), type = "l")
 
