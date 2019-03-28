46d9f24e7f3f32f1a794951e2110ad65e2bea3ca
Malavika Rajeev <mrajeev08@gmail.com>
2019-03-28 19:44:22 +0300

    functions for getting data changed to match SD/Bali pipeline

--- R/data_functions.R
+++ R/data_functions.R
@@ -45,32 +45,16 @@ get.campaigns.WM <- function(vacc = vacc_data, pop = pop_data, threshold, shape)
   return(campaigns)
 }
 
-# Getting gridded data -----------------------------------------------------
+# Getting demographic data -----------------------------------------------------
 ## Gets the village ID, starting pop, the proportion of the pop by village, 
-## HDR, and the birth rate for each cell in grid
-get.grid <- function(shapefile, resolution = 1000, pop = pop_data, census = census_data, 
-                     deaths = 0.44) {
-  # pop = pop_data; census = census_data; deaths = 0.44; shapefile = SD_shape;
+## HDR, and the growth rate for each cell in grid
+## Gets the village growth rate and starting pop for each village
+
+get.demdata <- function(shapefile, res_meters = 1000, pop = pop_data, census = census_data) {
+  ## To test code
+  pop = pop_data; census = census_data; res_meters = 1000; deaths = 0.44; shapefile = SD_shape;
   
-  ## 1. Get raster
-  r <- raster(shapefile)
-  res(r) <- 1000
-  SD_raster <- rasterize(shapefile, r)
-  rast_attributes <- SD_raster@data@attributes[[1]]
-  shapefile$ID_match <-rast_attributes$ID[match(shapefile$VILLCODE, rast_attributes$VILLCODE)]
-  
-  ## 2. Get proportion of dogs in each cell
-  census_dogs <- rasterize(cbind(census$X, census$Y), r,
-                           field = census$dogs + census$pups, fun = sum)
-  summarize_dogs <- as.data.frame(cbind(census_dogs@data@values, SD_raster@data@values))
-  summarize_dogs %>%
-    group_by(village_ID = V2) %>%
-    summarize(dogs_total_census = sum(V1, na.rm = TRUE)) %>%
-    right_join(shapefile@data, by = c("village_ID" = "ID_match")) -> shapefile@data
-  raster_pop <- rasterize(shapefile, r, field = shapefile$dogs_total_census)
-  prop <- census_dogs/raster_pop
-  
-  # 3. Get starting pop and births from village censuses 
+  # 1. Get starting pop and births from village censuses 
   pop$VILLCODES <- as.factor(pop$VILLCODES)
   shapefile@data %>%
     left_join(pop, by = c("VILLCODE" = "VILLCODES")) %>%
@@ -80,29 +64,60 @@ get.grid <- function(shapefile, resolution = 1000, pop = pop_data, census = cens
               villcode = VILLCODE[1]) %>%
     mutate(growth = exp(log(pop2012/pop2002)/10)) %>%
     right_join(shapefile@data, by = c("villcode" = "VILLCODE")) -> shapefile@data
-  shapefile$births <- deaths + (shapefile$growth - 1)
-
-  ## 4. Get HDR and starting dog population from starting vill population
-  census_humans <- rasterize(cbind(census$X, census$Y), r,
-                             field = census$Adults + census$Children, fun = sum)
-  HDR <- census_humans/census_dogs
-  births <- rasterize(shapefile, r, field = shapefile$births)
-  start_pop <- rasterize(shapefile, r, field = shapefile$pop2002)
-  values(start_pop) <- rbinom(ncell(start_pop), size = values(start_pop), 
-                              prob = 1/values(HDR)*values(prop))
   
-  ## 5. Return stacked raster file
-  dat <- as_tibble(cbind(SD_raster@data@values, HDR@data@values, births@data@values, 
-                             prop@data@values, start_pop@data@values))
-  colnames(dat) <- c("village_ID", "HDR", "births", "prop_pop", "start_pop")
-  dat$villcode <- rast_attributes$VILLCODE[match(dat$village_ID, rast_attributes$ID)]
-  dat$cell_id <- 1:nrow(dat) ## in order to use identity of cell!
-  return(dat)
+  if (is.numeric(res_meters)) {
+    ## 2. Get raster
+    r <- raster(shapefile)
+    res(r) <- res_meters
+    SD_raster <- rasterize(shapefile, r)
+    rast_attributes <- SD_raster@data@attributes[[1]]
+    shapefile$ID_match <-rast_attributes$ID[match(shapefile$villcode, rast_attributes$villcode)]
+    
+    ## 3. Get proportion of dogs in each cell (from census)
+    census_dogs <- rasterize(cbind(census$X, census$Y), r,
+                             field = census$dogs + census$pups, fun = sum)
+    summarize_dogs <- as.data.frame(cbind(census_dogs@data@values, SD_raster@data@values))
+    summarize_dogs %>%
+      group_by(village_ID = V2) %>%
+      summarize(dogs_total = sum(V1, na.rm = TRUE)) %>%
+      right_join(shapefile@data, by = c("village_ID" = "ID_match")) -> shapefile@data
+    raster_pop <- rasterize(shapefile, r, field = shapefile$dogs_total)
+    prop <- census_dogs/raster_pop
+  
+    ## 4. Get HDR and starting dog population from starting vill population
+    census_humans <- rasterize(cbind(census$X, census$Y), r,
+                               field = census$Adults + census$Children, fun = sum)
+    summarize_humans <- as.data.frame(cbind(census_humans@data@values, SD_raster@data@values))
+    summarize_humans %>%
+      group_by(village_ID = V2) %>%
+      summarize(humans_total = sum(V1, na.rm = TRUE)) %>%
+      right_join(shapefile@data, by = c("village_ID" = "village_ID")) -> shapefile@data
+    shapefile$HDR <- shapefile$humans_total/shapefile$dogs_total
+    shapefile$start_pop <- shapefile$pop2002/shapefile$HDR
+    HDR <- rasterize(shapefile, r, field = shapefile$HDR)
+    growth <- rasterize(shapefile, r, field = shapefile$growth) 
+    start_pop <- rasterize(shapefile, r, field = shapefile$start_pop) ## just the starting vill pops
+    
+    ## 5. Return stacked raster file
+    dat <- as_tibble(cbind(SD_raster@data@values, HDR@data@values, growth@data@values, 
+                               prop@data@values, start_pop@data@values))
+    colnames(dat) <- c("village_ID", "HDR", "growth", "prop_pop", "start_pop")
+    dat$start_pop <- rbinom(nrow(dat), size = round(dat$start_pop), prob = dat$prop_pop)
+    dat$villcode <- rast_attributes$villcode[match(dat$village_ID, rast_attributes$ID)]
+    dat$cell_id <- 1:nrow(dat) ## in order to use identity of cell!
+    return(dat)
+  } else {
+    ## For village data get HDR + start_pop
+    census %>%
+      group_by(Village.2002) %>%
+      summarize(dogs_total = sum(dogs + pups, na.rm = TRUE), 
+                humans_total = sum(Adults + Children, na.rm = TRUE), 
+                HDR = humans_total/dogs_total) %>%
+      right_join(shapefile@data, by = c("Village.2002" = "Village_2002")) %>% # keep all shapefile data
+      mutate(start_pop = pop2002/HDR) -> shapefile@data
+    return(shapefile@data)
+  }
 }
-
-
-# Cleaning census -----------------------------------------------------------------------------
-
 
 # Cleaning rabid  -----------------------------------------------------------------------------
 
