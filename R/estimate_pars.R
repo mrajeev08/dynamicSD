8efd12423c2e06f0f804c19148e315b03093e1ac
Malavika Rajeev <mrajeev08@gmail.com>
2021-03-17 01:49:43 -0700

    adding parameter estimation

---
+++ R/estimate_pars.R
@@ -0,0 +1,110 @@
+estimate_pars <- function(reftable, 
+                          par_names = c("R0", "k", "iota"), 
+                          exclude = c("stopped", "sim", "break_threshold"),
+                          ncores, 
+                          paral, 
+                          obs_data = NULL, 
+                          sampsize = nrow(reftable), 
+                          ntree = 500, 
+                          predict = TRUE, 
+                          return_training = FALSE) {
+  
+
+    reftab <- reftable[, !c(exclude), with = FALSE]
+    
+    modname <- unique(reftab$modname)
+    
+    if(length(modname) > 1) message("Warning: attempting to estimate params with more than one model!")
+    modname <- modname[1]
+    
+    reftab <- reftab[, !"modname", with = FALSE]
+    
+    if(sampsize < nrow(reftable)) {
+      # this will error if group sizes are not balanced!
+      reftab <- reftab[sample(.N, sampsize), ]
+    } 
+    
+    out <- 
+      foreach(i = seq_len(length(par_names)), .combine = comb, 
+            .multicombine = TRUE) %do% {
+      
+      
+      if(!par_names[i] %in% names(reftab)) {
+        
+        out <- NULL
+        
+      } else {
+        
+        reftab_i <- reftab[, !par_names[-i], with = FALSE]
+        setnames(reftab_i, par_names[i], "par")
+
+        par_obj <- regAbcrf(par~., data = reftab_i, ntree = ntree, 
+                            paral = paral, ncores = ncores)
+        
+        if(predict & !is.null(obs_data)) {
+          
+          out_preds <- out.regAbcrf(par_obj, obs = obs_data, 
+                                    training = reftab_i, 
+                                    paral = paral, 
+                                    ncores = ncores)
+          out_preds[, c("sim_id", "param") := .(1:.N, par_names[i])]
+
+        } else {
+          out_preds <- NULL
+        }
+        
+        err_abcrf <- data.table(err.regAbcrf(par_obj, reftab_i, paral = paral, 
+                                          ncores = ncores))
+        
+        var_imp <- par_obj$model.rf$variable.importance
+        var_imp <- data.table(setNames(stack(var_imp)[2:1], 
+                                       c('summstat','importance')))
+        out <- list(preds = out_preds, err = err_abcrf, var_imp = var_imp)
+        invisible(lapply(out, append_col, val = par_names[i], col_name = "param"))
+      }
+      out
+    }
+    
+   lapply(out, append_col, val = modname, col_name = "modname")
+    
+    if(return_training) {
+      return(list(out = out, training = training))
+    } else {
+      return(out)
+    }
+    
+}
+
+estimate_par_se <- function(reftable, 
+                           par_names = c("R0", "k", "iota"), 
+                           exclude = c("stopped", "sim", "break_threshold"),
+                           ncores, 
+                           paral, 
+                           obs_data = NULL, 
+                           ntree = 500, 
+                           predict = TRUE, 
+                           samp_prop = 0.75, 
+                           nsims = 5) {
+  
+  foreach(i = seq_len(nsims), .combine = comb, 
+          .multicombine = TRUE) %do% {
+            
+            out <- estimate_pars(reftable, 
+                                  par_names,
+                                  exclude,
+                                  ncores, 
+                                  paral, 
+                                  obs_data, 
+                                  sampsize = floor(samp_prop * nrow(reftable)), 
+                                  ntree, 
+                                  predict, 
+                                  return_training = FALSE)
+            
+            lapply(out, append_col, col_name = "nsim", val = i)
+            out
+  } -> out
+  
+  return(out)
+  
+}
+
